e92b6f27b846839dfdc1639b403e06a2d7a2c0fd
TrapCode <48396844+trapcodeio@users.noreply.github.com>
2022-03-23 13:48:38 +0100

    Update package keywords

--- bundle.js
+++ bundle.js
@@ -13,7 +13,7 @@ es.buildSync({
     legalComments: "none",
     treeShaking: true,
     minify: true,
-    external: ["abolish", "vue"]
+    external: ["abolish"]
 });
 
 const fileEsm = __dirname + "/index.esm-bundled.js";
